806148eae85849d169576dfe12f956a76d6c4f16
mikanfactory <k952i4j14x17@gmail.com>
2026-02-25 11:20:57 +0900

    feat: replace shell scripts with swap subcommands in CLI

--- scripts/swap-center.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-session=$(tmux display-message -p '#{session_name}')
-tmux swap-pane -d -s "${session}:main-window.0" -t "${session}:background-window.0"
-tmux swap-pane -d -s "${session}:background-window.0" -t "${session}:background-window.1"
--- scripts/swap-rb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-session=$(tmux display-message -p '#{session_name}')
-tmux swap-pane -d -s "${session}:main-window.2" -t "${session}:background-window.2"
-tmux swap-pane -d -s "${session}:background-window.2" -t "${session}:background-window.3"
